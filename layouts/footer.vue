d6f60a45570861c0e863060dcb212760c1080641
杨扬 <1076535690@qq.com>
2024-11-13 21:09:15 +0800

    feat(layout): 初步添加layout布局

---
+++ layouts/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>底部信息注解</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>头部信息的注解</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
