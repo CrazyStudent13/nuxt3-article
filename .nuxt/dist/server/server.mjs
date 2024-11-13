2933425712fc133c28ab0a6c50515c6e6ee46859
杨扬 <1076535690@qq.com>
2024-11-13 20:16:26 +0800

    chore: 初始化项目配置和组件定义
    - 添加 .gitignore 文件，忽略 node_modules 目录
    - 创建 .nuxt 目录并添加初始配置文件
    - 定义全局组件类型和导出

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///E:/gitPro/nuxt3-article/node_modules/.pnpm/@nuxt+vite-builder@3.14.159_@types+node@22.9.0_magicast@0.3.5_rollup@4.26.0_terser@5.36.0_typ_3hyaxf7grbyv73a7dhqxkvcf7u/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
