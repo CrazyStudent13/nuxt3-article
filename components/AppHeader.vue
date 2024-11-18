2b518fad62ed14fc759c105ff510ca3decda3842
杨扬 <1076535690@qq.com>
2024-11-18 16:46:25 +0800

    feat(components): 添加全局导航栏组件 AppHeader
    - 新增 AppHeader.vue 组件，包含网站导航功能
    - 导航项包括主页、攻略、新闻、游戏、关于等页面
    - 使用 NuxtLink 组件实现页面跳转
    - 样式使用 Less 语言编写，符合项目风格

---
+++ components/AppHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="header">
+    <nav class="inner" role="navigation">
+      <NuxtLink v-for="(item, key) in header.data" :key="key" :to="`${item.path}`">
+        {{ item.title }}
+      </NuxtLink>
+    </nav>
+  </header>
+</template>
+<script setup>
+const header = reactive({
+  data: [
+    { title: '主页', path: '/' },
+    { title: '攻略', path: '/article' },
+    { title: '新闻', path: '/news' },
+    { title: '游戏', path: '/ask' },
+    { title: '关于', path: '/about' }
+  ]
+})
+</script>
+
+<style lang="less" scoped>
+.header {
+  background-color: #020420;
+  z-index: 999;
+  height: 55px;
+
+  .inner {
+    max-width: 800px;
+    box-sizing: border-box;
+    margin: 0px auto;
+    padding: 12px 5px;
+    display: flex;
+    place-items: center;
+  }
+
+  & a {
+    color: #fff;
+    line-height: 24px;
+    transition: color 0.15s ease;
+    display: inline-block;
+    vertical-align: middle;
+    font-weight: 300;
+    letter-spacing: 0.075em;
+    margin-right: 1.8em;
+
+    &:hover {
+      color: #00dc82;
+    }
+
+    &.active {
+      color: #00dc82;
+    }
+
+    &:nth-child(6) {
+      margin-right: 0;
+    }
+  }
+}
+</style>
