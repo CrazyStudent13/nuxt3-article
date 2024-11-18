38aa70f6da9be8a68d381c09955db58858dca00f
杨扬 <1076535690@qq.com>
2024-11-18 14:27:26 +0800

    feat(index): 设置网页的 SEO 元数据和语言属性
    - 在 index.vue 中添加 script setup 部分
    - 使用 useServerHead 设置 HTML 属性为 zh-CN
    - 使用 useSeoMeta 设置网页的 SEO 元数据
      - titleTemplate: 游戏速递
      - description: 专注于游戏资讯，玩家攻略，游戏存档分享的小站，希望为玩家们提供更好的游戏体验。

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useServerHead({
+  htmlAttrs: { lang: 'zh-CN' }
+})
+useSeoMeta({
+  titleTemplate: '游戏速递',
+  description: '专注于游戏资讯，玩家攻略，游戏存档分享的小站，希望为玩家们提供更好的游戏体验。'
+})
+</script>
