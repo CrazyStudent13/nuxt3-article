ecf4b5acb40c56e73ea3c8cdce2e302f051813cc
杨扬 <1076535690@qq.com>
2025-05-28 21:53:42 +0800

    feat(archive): 添加存档列表页面
    - 暂时先放个demo

---
+++ pages/archive/list.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="list">
+    <div class="list-main">
+      <div v-for="(item, index) in article.list" :key="index" @click="article.handleRowClick(item)">
+        <article-list-card :card-info="item" />
+      </div>
+    </div>
+
+    <div class="list-pagination">
+      <el-pagination
+        v-model:current-page="article.pageNum"
+        v-model:page-size="article.pageSize"
+        :page-sizes="[10, 20, 30, 40, 50]"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="article.total"
+        @size-change="article.handleSizeChange"
+        @current-change="article.handleCurrentChange"
+      />
+    </div>
+
+    <el-backtop :right="50" :bottom="50" />
+  </div>
+</template>
+
+<script setup>
+import { getArticleList } from '@/api/article'
+
+const article = reactive({
+  list: [],
+  pageSize: 10,
+  pageNum: 1,
+  total: 0,
+  request: async () => {
+    const params = {
+      title: '',
+      pageNum: article.pageNum,
+      pageSize: article.pageSize
+    }
+
+    const result = await getArticleList(params)
+
+    article.total = result.total
+    article.list = result.list
+  },
+  handleRowClick: (item) => {
+    useRouter().push({
+      path: '/article/detail',
+      query: {
+        id: item.articleId
+      }
+    })
+  },
+  handleSizeChange: (val) => {
+    article.pageSize = val
+    article.request()
+  },
+  handleCurrentChange: (val) => {
+    article.pageNum = val
+    article.request()
+  }
+})
+
+// 调用请求方法以获取数据
+article.request()
+</script>
+
+<style lang="less" scoped>
+.list {
+  margin: 20px 0;
+  &-pagination {
+    padding: 8px 16px;
+    background: #ffffff;
+    text-align: center;
+  }
+}
+</style>
