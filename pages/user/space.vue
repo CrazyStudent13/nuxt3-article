1ece5af5f66d3334b11dba09156a4d65f1f2477f
杨扬 <1076535690@qq.com>
2025-06-03 00:11:37 +0800

    feat(user): 个人空间页面初步搭建
    - 添加用户信息展示区域，包括头像、用户名、UID、邮箱和注册时间
    - 实现个人信息描述列表
    - 添加带标签页的文章、评论、收藏和点赞功能区域
    - 优化页面样式，使用 Element UI 组件

--- pages/user/space.vue
+++ pages/user/space.vue
@@ -1,8 +1,96 @@
 <template>
-  <!-- 个人中心 -->
-  <div class="main-container">个人中心，暂时先不忙做，到时候参考掘金的中心</div>
+  <div class="main-container">
+    <el-card shadow="none" :body-style="{ padding: '20px' }">
+      <el-row :gutter="20">
+        <el-col :span="3">
+          <el-avatar :size="60" src="https://empty" class="avatar" @error="errorHandler">
+            <img :src="userInfo.avatar" />
+          </el-avatar>
+        </el-col>
+        <el-col :span="21">
+          <el-descriptions :column="3">
+            <template #title>
+              <h3>{{ userInfo.name }}</h3>
+            </template>
+            <el-descriptions-item label="Uid">{{ userInfo.Uid }}</el-descriptions-item>
+            <el-descriptions-item label="注册邮箱">{{ userInfo.email }}</el-descriptions-item>
+            <el-descriptions-item label="注册时间">{{ userInfo.registerTime }}</el-descriptions-item>
+            <el-descriptions-item>
+              {{ userInfo.signature }}
+            </el-descriptions-item>
+          </el-descriptions>
+        </el-col>
+      </el-row>
+    </el-card>
+
+    <br />
+
+    <el-tabs v-model="activeName" type="border-card" tab-position="top">
+      <el-tab-pane v-for="item in panes" :key="item.key" :label="item.label" :name="item.key">
+        {{ item.name }}
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const userInfo = {
+  name: '叶远川',
+  avatar: 'https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png',
+  Uid: '1796629',
+  email: '10000000000@qq.com',
+  registerTime: '2021-01-01',
+  signature: '不过是些许风霜罢了'
+}
 
-<style scoped></style>
+const activeName = ref('1')
+
+const panes = [
+  {
+    key: '1',
+    label: '文章',
+    name: 'article'
+  },
+  {
+    key: '2',
+    label: '评论',
+    name: 'comment'
+  },
+  {
+    key: '3',
+    label: '收藏',
+    name: 'favorite'
+  },
+  {
+    key: '4',
+    label: '点赞'
+  }
+]
+
+const errorHandler = () => {
+  return true
+}
+</script>
+
+<style lang="less" scoped>
+.avatar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 0 auto;
+}
+
+.userInfo {
+  display: flex;
+  flex-wrap: wrap;
+  &-item {
+    width: 100%;
+    margin: 4px 0;
+
+    .icon {
+      margin: 2px;
+    }
+  }
+}
+</style>
